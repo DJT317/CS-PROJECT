02ec28c3d0b0c1ef78c14474b1a7f5035ad7a141
rssssssssssssssssssssssssssssssssssss <roshinisunjith@gmail.com>
2024-12-07 03:42:35 -0500

    Added JS
    Added Project.js + changed the style of Acetate-Sheets.html and Balsa-Wood.html to match that of the other 3 material pages + added dropdown options and made all material pages work with JS

---
+++ Project.js
@@ -0,0 +1,169 @@
+//1, 2, 3 are chip board
+
+function cart1DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart2DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart3DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+//the following are cardstock
+
+function cart4DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart5DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart6DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart7DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart8DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart9DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+// 10, 11, 12, 13 are acetate sheets
+
+function cart10DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart11DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart12DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart13DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+//the following are foam board
+
+function cart14DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart15DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart16DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+function cart17DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart18DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart19DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+// the rest are balsa wood
+
+function cart20DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart21DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart22DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
+
+function cart23DataSave(productName, productNumber, productSize){
+    localStorage.setItem('saveProductName', productName);
+    localStorage.setItem('saveProductNumber', productNumber);
+    localStorage.setItem('saveProductSize', productSize);
+    window.location.href = 'Buy.html';
+}
